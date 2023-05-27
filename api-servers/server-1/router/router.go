492de49b279f25cb5869fe63f9804a19bf64e6eb
lucas <lucasdearaujo.brandao@gmail.com>
2023-05-27 20:12:17 -0300

    remove verify token from routers

--- api-servers/server-1/router/router.go
+++ api-servers/server-1/router/router.go
@@ -19,7 +19,6 @@ func StartApiEngine() {
 	httpEngine := gin.Default()
 	httpEngine.GET(fmt.Sprintf("%s/health", config.DEFAULT_PATH), handlers.HealthCheck)
 	httpEngine.GET(fmt.Sprintf("%s/authenticate", config.DEFAULT_PATH), handlers.DeliveryToken)
-	httpEngine.POST(fmt.Sprintf("%s/checkToken", config.DEFAULT_PATH), handlers.VerifyToken)
 	httpEngine.POST(fmt.Sprintf("%s/create", config.DEFAULT_PATH), handlers.CreateUser)
 	
 	// apiGateWayHandShake := httpEngine.Group("/")
