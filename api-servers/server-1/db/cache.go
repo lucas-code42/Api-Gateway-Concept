36294b339511243bd601a707c19215168d3da5e4
lucas <lucasdearaujo.brandao@gmail.com>
2023-05-27 00:40:51 -0300

    cache layer

--- api-servers/server-1/db/cache.go
+++ api-servers/server-1/db/cache.go
@@ -1,27 +1,58 @@
 package db
 
-import "github.com/go-redis/redis"
+import (
+	"context"
+	"time"
+
+	"github.com/api-server/lcs42/models"
+	"github.com/go-redis/redis"
+)
 
 type CacheDb struct {
-	db *redis.Client
+	Db  *redis.Client
+	Ctx *context.Context
 }
 
-func InitCacheDb(db *redis.Client) *CacheDb {
-	return &CacheDb{db}
+type Cache interface {
+	Get() error
+	GetAll() error
+	Create() error
+	Update() error
+	Delete() error
 }
 
-func (c *CacheDb) Get() {
-
+func NewCacheDb(redisClient *redis.Client, context *context.Context) *CacheDb {
+	return &CacheDb{Db: redisClient, Ctx: context}
 }
 
-func (c *CacheDb) GetAll() {
-
+func (c *CacheDb) Create(user models.User) error {
+	err := c.Db.Set(
+		"user",
+		map[string]string{
+			"userId":   user.Id,
+			"userName": user.Name,
+			"userEmal": user.Email,
+		},
+		time.Duration(time.Minute*10),
+	)
+	if err != nil {
+		return err.Err()
+	}
+	return nil
 }
 
-func (c *CacheDb) Delete() {
-
+func (c *CacheDb) Get() error {
+	return nil
 }
 
-func (c *CacheDb) Update() {
+func (c *CacheDb) GetAll() error {
+	return nil
+}
 
+func (c *CacheDb) Delete() error {
+	return nil
+}
+
+func (c *CacheDb) Update() error {
+	return nil
 }
