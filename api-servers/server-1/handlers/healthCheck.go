534e08abb938b172328861f5ac7e3a845ed10f3e
lucas <lucasdearaujo.brandao@gmail.com>
2023-05-27 20:09:00 -0300

    change map for gin.H{}

--- api-servers/server-1/handlers/healthCheck.go
+++ api-servers/server-1/handlers/healthCheck.go
@@ -8,5 +8,5 @@ import (
 
 // HealthCheck just health checker
 func HealthCheck(c *gin.Context) {
-	c.JSON(200, map[string]string{"server1": time.Now().Format("2017.09.07 17:06:06")})
+	c.JSON(200, gin.H{"server1": time.Now().Format("2017.09.07 17:06:06")})
 }
