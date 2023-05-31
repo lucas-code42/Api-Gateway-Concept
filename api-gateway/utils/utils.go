cf6460ab84d806f663c3b4ec078eb3ec3e8e1fd0
lucas <lucasdearaujo.brandao@gmail.com>
2023-05-30 23:20:33 -0300

    add utils package

---
+++ api-gateway/utils/utils.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"Api-Gateway-lcs42/models"
+	"encoding/json"
+)
+
+func ParseDtoResponse(data []byte) (interface{}, error) {
+	var buffer interface{}
+	if err := json.Unmarshal(data, &buffer); err != nil {
+		return models.DtoResponse{}, err
+	}
+	return buffer, nil
+}
+
+// TODO: improve the default response
+// think of a way how to use dto model
+
+// func MountResponse(message string, data []interface{}, stausCode int) map[string]any {
+// 	return gin.H{
+// 		"data": models.DtoResponse{
+// 			Message:    message,
+// 			Data:       data,
+// 			Id:         uuid.NewString(),
+// 			StatusCode: stausCode,
+// 			Time:       time.Now().Format("2017.09.07 17:06:06"),
+// 		},
+// 	}
+// }
