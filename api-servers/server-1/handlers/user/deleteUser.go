45c29ac49e73e27e53d96e2b382c863e19ab5500
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-11 17:33:50 -0300

    add new handler delete user

---
+++ api-servers/server-1/handlers/user/deleteUser.go
@@ -0,0 +1,34 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/api-server/lcs42/db"
+	"github.com/api-server/lcs42/handlers"
+	"github.com/api-server/lcs42/handlers/security"
+	"github.com/api-server/lcs42/models"
+	"github.com/gin-gonic/gin"
+)
+
+func DeleteUser(c *gin.Context) {
+	if security.VerifyToken(c.Request.Header.Get("Authorization")) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": handlers.UNAUTHORIZED})
+		return
+	}
+
+	user := models.NewUser()
+	if !user.ValidadeUserStruct(models.DELETE_USER) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": handlers.BAD_REQUEST})
+		return
+	}
+
+	rds := db.MountRds()
+	defer rds.CloseRds()
+
+	if err := rds.Delete(user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": handlers.INTERNAL_SERVER_ERROR})
+		return
+	}
+
+	c.JSON(http.StatusAccepted, gin.H{"sucess": true})
+}
