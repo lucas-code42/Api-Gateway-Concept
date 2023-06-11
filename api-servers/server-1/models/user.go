44bc81f456397ad4bc11a95c3eea51b9cc165e00
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-11 00:10:54 -0300

    redis annotation

--- api-servers/server-1/models/user.go
+++ api-servers/server-1/models/user.go
@@ -2,7 +2,7 @@ package models
 
 // User model
 type User struct {
-	Id    string `json:"id,omitempty"`
-	Name  string `json:"name" binding:"required"`
-	Email string `json:"email" binding:"required"`
+	Id    string `json:"id,omitempty" redis:"userId"`
+	Name  string `json:"name" binding:"required" redis:"userName"`
+	Email string `json:"email" binding:"required" redis:"userEmail"`
 }
