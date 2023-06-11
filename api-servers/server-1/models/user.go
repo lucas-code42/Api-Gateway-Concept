5f829b6ac12e5fa2a152ca743d0ab1f052b70015
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-11 17:36:05 -0300

    validate struct

--- api-servers/server-1/models/user.go
+++ api-servers/server-1/models/user.go
@@ -1,11 +1,76 @@
 package models
 
+import (
+	"fmt"
+	"net/mail"
+	"strings"
+	"unicode"
+
+	"github.com/google/uuid"
+)
+
 // User model
 type User struct {
-	Id    string `json:"id,omitempty" redis:"userId"`
-	Name  string `json:"name" binding:"required" redis:"userName"`
-	Email string `json:"email" binding:"required" redis:"userEmail"`
+	Id    string `json:"id,omitempty"`
+	Name  string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required"`
 }
 
+var (
+	UPDATE_USER = "UPDATE"
+	DELETE_USER = "DELETE"
+	GET_USER    = "GET"
+)
 
-// TODO: CREATE METHODS TO VALIDADE ALL FIELDS
+func NewUser() *User {
+	return &User{}
+}
+
+func (u *User) ValidadeUserStruct(handler string) bool {
+	switch handler {
+	case UPDATE_USER:
+		validUiid, err := uuid.Parse(u.Id)
+		if err != nil {
+			return false
+		}
+		u.Id = validUiid.String()
+
+	case GET_USER:
+		_, err := uuid.Parse(u.Id)
+		if err != nil {
+			return false
+		} else {
+			fmt.Println("aqui***")
+			return true
+		}
+
+	case DELETE_USER:
+		_, err := uuid.Parse(u.Id)
+		if err != nil {
+			return false
+		}
+	}
+
+	validAddr, err := mail.ParseAddress(u.Email)
+	if err != nil {
+		return false
+	}
+	u.Email = strings.TrimSpace(validAddr.Address)
+
+	if onlyLetters(u.Name) {
+		u.Name = strings.TrimSpace(u.Name)
+	} else {
+		return false
+	}
+
+	return true
+}
+
+func onlyLetters(data string) bool {
+	for _, v := range data {
+		if !unicode.IsLetter(v) {
+			return false
+		}
+	}
+	return true
+}
