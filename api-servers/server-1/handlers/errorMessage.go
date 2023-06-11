bd5095b03bb3be855bb62a822bab22c79c5dff99
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-11 17:33:20 -0300

    defalut error msgs

---
+++ api-servers/server-1/handlers/errorMessage.go
@@ -0,0 +1,7 @@
+package handlers
+
+var (
+	UNAUTHORIZED          = "unauthorized user"
+	INTERNAL_SERVER_ERROR = "Internal server error, try again later"
+	BAD_REQUEST           = "looks like you made a bad request"
+)
