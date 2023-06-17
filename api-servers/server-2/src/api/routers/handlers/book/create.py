97faaa218f9f6e770281eee1d574c7c834642a3d
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-17 12:33:36 -0300

    ft: create handler and repository method

--- api-servers/server-2/src/api/routers/handlers/book/create.py
+++ api-servers/server-2/src/api/routers/handlers/book/create.py
@@ -1,8 +1,31 @@
 from fastapi import APIRouter
+from src.api.exceptions import ApiFailedConnectDataBase
+from src.api.model.books import BooksModels
+from src.db.db import PostgresConnection
+from src.db.repository.book import BookRepository
 
 create = APIRouter()
 
 
-@create.post("/")
-async def create_handler():
-    return
+@create.post("/", response_model=BooksModels, status_code=200)
+async def create_handler(book: BooksModels):
+    pg = None
+    response = None
+
+    try:
+        pg = PostgresConnection()
+        pg._connect_db()
+
+        repository = BookRepository(pg.conn)
+        if repository.create_book(book):
+            response = book
+        else:
+            response = {}
+
+    except ApiFailedConnectDataBase:
+        return "erro"
+    finally:
+        if isinstance(pg, PostgresConnection):
+            pg._close_connection()
+
+    return response
--- api-servers/server-2/src/db/repository/book.py
+++ api-servers/server-2/src/db/repository/book.py
@@ -1,11 +1,34 @@
-from src.db.db import Postgres
-from psycopg2 import connection
+
+from src.api.model.books import BooksModels
+from psycopg2.errors import Error
+import psycopg2
 
 
 class BookRepository:
-    def __init__(self, db_connection: connection) -> None:
+    def __init__(self, db_connection: psycopg2.connect) -> None:
         self.pg_connection = db_connection
         self.cursor = self.pg_connection.cursor()
 
-    def create_book(self):
-        self.cursor.execute("")
+    def create_book(self, book: BooksModels) -> bool:
+        result = False
+        try:
+            query = """
+                INSERT INTO 
+                    books (name, price, author)
+                VALUES 
+                    (%s, %s,%s)
+            """
+            insert_data = (book.name, book.price, book.author)
+
+            self.cursor.execute(query, insert_data)
+            self.pg_connection.commit()
+
+            if self.cursor.rowcount > 0:
+                result = True
+        except Error as e:
+            print(e)
+        finally:
+            if self.cursor:
+                self.cursor.close()
+
+        return result
