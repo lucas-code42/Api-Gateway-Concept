a8181fd94d7477d2381f00ec96e3aa683359052f
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-18 22:23:57 -0300

    ft: new handler jwt

---
+++ api-servers/server-2/src/api/routers/handlers/jwt/jwt_token.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from src.api.security.jwt_token.token import generate_jwt_token
+
+jwt = APIRouter()
+
+
+class JwtToken(BaseModel):
+    token: str
+
+
+@jwt.get("/", response_model=JwtToken)
+def delivery_token() -> any:
+    return JwtToken(**{"token": generate_jwt_token()})
