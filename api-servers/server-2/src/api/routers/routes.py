e684fe2b91b8dd5d2609f456b9d48e1c634e73a0
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-18 21:54:25 -0300

    ft: ajust routers

--- api-servers/server-2/src/api/routers/routes.py
+++ api-servers/server-2/src/api/routers/routes.py
@@ -4,6 +4,7 @@ from src.api.routers.handlers.book.create import create
 from src.api.routers.handlers.book.delete import delete
 from src.api.routers.handlers.book.get import get
 from src.api.routers.handlers.book.update import update
+from src.api.routers.handlers.jwt.jwt_token import jwt
 
 
 endpoints = APIRouter()
@@ -11,3 +12,4 @@ endpoints.include_router(router=create, prefix="/book")
 endpoints.include_router(router=delete, prefix="/book")
 endpoints.include_router(router=get, prefix="/book")
 endpoints.include_router(router=update, prefix="/book")
+endpoints.include_router(router=jwt, prefix="/authentication")
