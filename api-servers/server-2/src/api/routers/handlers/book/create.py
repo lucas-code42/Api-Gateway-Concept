9771c9809dbf04bd9e8a8a895977ae7c3efed72f
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-18 21:51:28 -0300

    ft: create handler

--- api-servers/server-2/src/api/routers/handlers/book/create.py
+++ api-servers/server-2/src/api/routers/handlers/book/create.py
@@ -1,31 +1,28 @@
-from fastapi import APIRouter
-from src.api.exceptions import ApiFailedConnectDataBase
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from src.api.exceptions import ApiFailedConnectDataBase, ApiFailedToInsertBook
 from src.api.model.books import BooksModels
-from db.database import PostgresConnection
+from src.db.database import PostgresConnection
 from src.db.repository.book import BookRepository
 
 create = APIRouter()
 
 
-@create.post("/", response_model=BooksModels, status_code=200)
-async def create_handler(book: BooksModels):
+@create.post("/", response_model=BooksModels)
+async def create_handler(book: BooksModels) -> any:
     pg = None
-    response = None
-
     try:
         pg = PostgresConnection()
         pg._connect_db()
-
         repository = BookRepository(pg.conn)
-        if repository.create_book(book):
-            response = book
-        else:
-            response = {}
-
+        if not repository.create_book(book):
+            raise ApiFailedToInsertBook
     except ApiFailedConnectDataBase:
-        return "erro"
+        return HTTPException(status_code=500, detail="can't connect to database")
+    except ApiFailedToInsertBook:
+        return HTTPException(status_code=500, detail="can't insert to database")
     finally:
-        if isinstance(pg, PostgresConnection):
+        if pg is not None:
             pg._close_connection()
-
-    return response
+    return JSONResponse(content=book, status_code=201)
