c7d95b339efaa6b129c7e68a14eae6bfdf96ba75
lucas <lucasdearaujo.brandao@gmail.com>
2023-06-27 23:24:46 -0300

    fx: fix raise exceptions

--- api-servers/server-2/src/api/routers/handlers/book/create.py
+++ api-servers/server-2/src/api/routers/handlers/book/create.py
@@ -6,11 +6,12 @@ from src.api.model.books import BooksModels
 from src.db.database import PostgresConnection
 from src.db.repository.book import BookRepository
 
+
 create = APIRouter()
 
 
 @create.post("/", response_model=BooksModels)
-async def create_handler(book: BooksModels) -> any:
+async def create_handler(book: BooksModels):
     pg = None
     try:
         pg = PostgresConnection()
@@ -19,10 +20,11 @@ async def create_handler(book: BooksModels) -> any:
         if not repository.create_book(book):
             raise ApiFailedToInsertBook
     except ApiFailedConnectDataBase:
-        return HTTPException(status_code=500, detail="can't connect to database")
+        raise HTTPException(
+            status_code=500, detail="can't connect to database")
     except ApiFailedToInsertBook:
-        return HTTPException(status_code=500, detail="can't insert to database")
+        raise HTTPException(status_code=500, detail="can't insert to database")
     finally:
-        if pg is not None:
+        if pg.conn is not None:
             pg._close_connection()
     return JSONResponse(content=book, status_code=201)
