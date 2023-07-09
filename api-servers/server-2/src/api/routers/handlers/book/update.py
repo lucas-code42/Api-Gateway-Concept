f04702c2632e7ae62b8f08132197cf3446f66f30
Lucas <lucasdearaujo.brandao@gmail.com>
2023-07-09 00:24:59 -0300

    ft: new route

--- api-servers/server-2/src/api/routers/handlers/book/update.py
+++ api-servers/server-2/src/api/routers/handlers/book/update.py
@@ -1,9 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from src.api.model.books import BooksModels
+from src.db.database import PostgresConnection
+from src.db.repository.book import BookRepository
+from src.api.exceptions import ApiFailedConnectDataBase, ApiFailedToVerifyId, ApiFailedToUpdateBooks
+from fastapi.encoders import jsonable_encoder
 
 update = APIRouter()
 
 
-@update.put("/")
-async def update_handler():
-    return
+@update.put("/", response_model=BooksModels)
+async def update_handler(book: BooksModels):
+    pg = None
+    result = None
+    try:
+        pg = PostgresConnection()
+        await pg._connect_db()
+        repository = BookRepository(pg.conn)
+        result = await repository.udpate_book_by_id(book)
+    except ApiFailedConnectDataBase:
+        raise HTTPException(
+            status_code=500, detail="can't connect to database")
+    except ApiFailedToVerifyId:
+        raise HTTPException(
+            status_code=500, detail="book id not exist")
+    except ApiFailedToUpdateBooks:
+        raise HTTPException(
+            status_code=500, detail="can't update book")
+    finally:
+        if pg.conn is not None:
+            await pg._close_connection()
+    return JSONResponse(content=jsonable_encoder(result), status_code=201)
