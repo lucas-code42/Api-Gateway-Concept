d27d1ded4909d5618f7a6351e5ed81bff0517000
Lucas <lucasdearaujo.brandao@gmail.com>
2023-07-26 22:02:10 -0300

    new package name

--- api-gateway/routers/server1/router.go
+++ api-gateway/routers/server1/router.go
@@ -1,13 +1,13 @@
 package server1
 
 import (
-	"Api-Gateway-lcs42/routers/server1/handlersServer1"
+	"Api-Gateway-lcs42/routers/server1/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Server1(rg *gin.RouterGroup) {
 	server1 := rg.Group("/")
-	server1.GET("/health", handlersServer1.HealthCheck)
-	server1.GET("/createUser", handlersServer1.CreateUser)
+	server1.GET("/health", handlers.HealthCheck)
+	server1.GET("/createUser", handlers.CreateUser)
 }
