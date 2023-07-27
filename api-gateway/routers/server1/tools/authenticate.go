f02a45f826515fda421942cfb5cf4ccb0eff7d72
Lucas <lucasdearaujo.brandao@gmail.com>
2023-07-26 22:00:17 -0300

    new package name

---
+++ api-gateway/routers/server1/tools/authenticate.go
@@ -0,0 +1,56 @@
+package tools
+
+import (
+	"Api-Gateway-lcs42/config"
+	"Api-Gateway-lcs42/models"
+	"Api-Gateway-lcs42/utils"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetJwt(server string) (*models.AuthJwt, error) {
+	var url string
+
+	switch server {
+	case "server1":
+		url = fmt.Sprintf("%s/authenticate", config.DEFAULT_HOST_SERVER1)
+	default:
+		// TODO: ajustar para outros servidores (para o 2 por ex)
+		url = fmt.Sprintf("%s/authenticate", config.DEFAULT_HOST_SERVER1)
+	}
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	req.Header.Set("Authorization", config.SERVER1_AUTH_KEY)
+
+	if err != nil {
+		return &models.AuthJwt{}, errors.New("error to get jwt")
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return &models.AuthJwt{}, errors.New("error to get jwt")
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return &models.AuthJwt{}, errors.New("error to get jwt")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return &models.AuthJwt{}, errors.New("error to get jwt")
+	}
+
+	response, err := utils.ParseJwt(body)
+	if err != nil {
+		return &models.AuthJwt{}, errors.New("error to get jwt")
+	}
+
+	return response, nil
+}
