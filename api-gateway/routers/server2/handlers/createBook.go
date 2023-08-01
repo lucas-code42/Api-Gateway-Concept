20b5cb1f52a94a679b1154f60760e9057bf7786d
Lucas <lucasdearaujo.brandao@gmail.com>
2023-07-31 23:26:35 -0300

    server 2 interaction

--- api-gateway/routers/server2/handlers/createBook.go
+++ api-gateway/routers/server2/handlers/createBook.go
@@ -1,13 +1,28 @@
 package handlers
 
 import (
+	"Api-Gateway-lcs42/config"
 	"Api-Gateway-lcs42/routers/server2/tools"
-	"fmt"
+	"bytes"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateBook(c *gin.Context) {
-	fmt.Println(tools.GetAuthentication())
-	c.JSON(200, gin.H{"data": true})
+	var clientJson interface{}
+	if err := c.BindJSON(&clientJson); err != nil {
+		c.JSON(400, gin.H{"err": "unprocessable entity"})
+		return
+	}
+
+	clientData := new(bytes.Buffer)
+	json.NewEncoder(clientData).Encode(clientJson)
+
+	r, err := tools.RequestServer2("POST", config.SERVER1_PATH, clientData)
+	if err != nil {
+		c.JSON(500, gin.H{"err": "Internal server error"})
+		return
+	}
+	c.JSON(201, gin.H{"data": r})
 }
