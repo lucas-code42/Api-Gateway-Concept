a4374cd36d17b55e013149f124470175edda4dee
Lucas <lucasdearaujo.brandao@gmail.com>
2023-08-04 23:26:30 -0300

    format

--- api-gateway/models/models.go
+++ api-gateway/models/models.go
@@ -10,7 +10,6 @@ type DtoResponse struct {
 	ExecutionTime time.Duration `json:"miliSeconds"`
 }
 
-// TODO ajustar auth de uma forma que fique instanciado em apenas uma variável
 type AuthJwt struct {
 	Token string `json:"token"`
 }
