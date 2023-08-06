27644cef27b0734cc5dd37b6c7c23fdf2ecf1f8e
Lucas <lucasdearaujo.brandao@gmail.com>
2023-08-05 23:11:28 -0300

    new method

--- api-gateway/routers/handlers/post.go
+++ api-gateway/routers/handlers/post.go
@@ -1,7 +1,42 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"Api-Gateway-lcs42/routers/tools"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func ServerInterfacePost(c *gin.Context) {
+	start := time.Now()
 
+	// ? PrepareRequest deveria lidar com o paylod?
+	var clientJson interface{}
+	if err := c.BindJSON(&clientJson); err != nil {
+		c.JSON(400, gin.H{"err": "unprocessable entity"})
+		return
+	}
+	clientData := new(bytes.Buffer)
+	json.NewEncoder(clientData).Encode(clientJson)
+
+	serverHost, err := PrepareRequest(c, POST)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+		return
+	}
+
+	r, err := tools.DoRequest(
+		serverHost.Url, serverHost.Path, fmt.Sprintf("%v", c.Keys["jwt"]), "POST", clientData,
+	)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+		return
+	}
+
+	r.ExecutionTime = time.Duration(time.Since(start).Milliseconds())
+	c.JSON(http.StatusOK, gin.H{"data": r})
 }
