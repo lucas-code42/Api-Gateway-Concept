b806c3107e5185d1cb8e796c159b261bb1a86457
Lucas <lucasdearaujo.brandao@gmail.com>
2023-08-14 21:46:44 -0300

    isoleted models

--- api-gateway/models/models.go
+++ api-gateway/models/models.go
@@ -1,7 +1,19 @@
 package models
 
-import "time"
+import (
+	"bytes"
+	"time"
+)
 
+// const para definir métodos http
+const (
+	GET    = 1
+	POST   = 2
+	PUT    = 3
+	DELETE = 4
+)
+
+// DtoResponse dto padrão de saída da api
 type DtoResponse struct {
 	Message       string        `json:"message"`
 	Id            string        `json:"id"`
@@ -10,6 +22,16 @@ type DtoResponse struct {
 	ExecutionTime time.Duration `json:"miliSeconds"`
 }
 
+// AuthJwt
 type AuthJwt struct {
 	Token string `json:"token"`
+}
+
+// RequestHost conjunto de dados para fazer uma request a um servidor
+type RequestHost struct {
+	Url     string
+	Path    string
+	Token   string
+	Method  string
+	Payload *bytes.Buffer
 }
